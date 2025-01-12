612366ffabe695c259e66b0a0c96b82868972afa
ghosh-trisha <trisha.ghosh1620@gmail.com>
2025-01-12 21:11:09 +0530

    hello world

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,22 @@
-// tailwind.config.js
+import type { Config } from "tailwindcss";
 const {nextui} = require("@nextui-org/react");
 
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+
+export default {
   content: [
-    // ...
-    // make sure it's pointing to the ROOT node_module
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
   },
   darkMode: "class",
   plugins: [nextui()],
-};
+} satisfies Config;
