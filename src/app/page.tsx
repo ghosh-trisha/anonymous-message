e4780fbea164e8725570aa9a8bd42d38b7900a1b
ghosh-trisha <trisha.ghosh1620@gmail.com>
2025-01-12 21:16:03 +0530

    hello world

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-
+import Test from './test';
 
 export default function Home() {
   return (
     <>
     <div className="text-5xl font-bold text-pink-800 bg-slate-500">Hello world</div>
+    <Test />
     </>
   );
 }
---
+++ src/app/test.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Test() {
+    return (
+      <>
+      <div className="text-5xl font-bold text-pink-800 bg-slate-200">Test</div>
+      </>
+    );
+  }
+  
